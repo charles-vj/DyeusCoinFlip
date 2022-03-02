0bbf55310cf6ab3ec5404e4aaadf53229ca27ac4
charles-vj <charles2019@iiitkottayam.ac.in>
2022-03-03 03:11:42 +0530

    added dotenv support

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
-
-const HARMONY_PRIVATE_KEY = "0x0610b6d30be50f100c93975b719d5ead6c928dcd6aaeb47352cb975170b6cbe5"
+require('dotenv').config()
 
 module.exports = {
   solidity: "0.8.10",
   networks: {
     testnet: {
       url: `https://api.s0.b.hmny.io`,
-      accounts: [`${HARMONY_PRIVATE_KEY}`]
+      accounts: [`${process.env.HARMONY_PRIVATE_KEY}`]
     },
     mainnet: {
       url: `https://api.harmony.one`,
-      accounts: [`${HARMONY_PRIVATE_KEY}`]
+      accounts: [`${process.env.HARMONY_PRIVATE_KEY}`]
     }
   }
 };
